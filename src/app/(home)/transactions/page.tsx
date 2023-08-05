2de768fc18782830ebe2542812872b7031c5581e
Mimmi <bjarmiae@gmail.com>
2023-08-05 02:06:32 +0000

    style: change header component

--- src/app/(home)/transactions/page.tsx
+++ src/app/(home)/transactions/page.tsx
@@ -1,15 +1,16 @@
 import createClient from '@/utils/supabase-server';
 import { getTransactions } from '@/utils/supabase_queries/transaction';
 import TransactionTable from './transactionTable';
+import Header from '@/components/Header/Header';
 
 export default async function HomePage() {
   const supabase = createClient();
   const transactions = await getTransactions(supabase);
 
   return (
-    <div className="pt-10">
-      <h1>Transactions</h1>
+    <>
+      <Header title="Hreyfingaryfirlit" />
       <TransactionTable initialData={transactions} />
-    </div>
+    </>
   );
 }
