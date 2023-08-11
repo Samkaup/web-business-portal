43c63d15c933faa1da30660856119fc127c7d642
Mimmi <bjarmiae@gmail.com>
2023-08-11 08:03:34 +0000

    feat: add new row type that support both table and view rows

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,18 @@ import { SupabaseClient } from '@supabase/supabase-js';
 import { Database } from './lib/database.types';
 
 export type AppSupabaseClient = SupabaseClient<Database>;
+
 export type TableName = keyof Database['public']['Tables'];
+export type ViewName = keyof Database['public']['Views'];
+
+export type RowName = TableName | ViewName;
+
 export type TableRow<T extends TableName> =
   Database['public']['Tables'][T]['Row'];
+export type ViewRow<T extends ViewName> = Database['public']['Views'][T]['Row'];
+
+export type Row<T extends RowName> = T extends TableName
+  ? TableRow<T>
+  : T extends ViewName
+  ? ViewRow<T>
+  : never;
