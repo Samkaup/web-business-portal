1fa748d109b15b1efa0c40fe86c43d05bd84228d
Mimmi <bjarmiae@gmail.com>
2023-08-11 07:49:44 +0000

    feat: remove supense since next does it for us

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,8 +1,6 @@
 import Footer from '@/components/Footer/Footer';
 import Navigation from '@/components/Navigation/Navigation';
 import '@/app/globals.css';
-import { Suspense } from 'react';
-import Loading from './loading';
 
 export default async function HomeLayout({
   children,
@@ -15,7 +13,7 @@ export default async function HomeLayout({
       <div className="flex flex-col min-h-screen">
         <main className="flex-1">
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 ">
-            <Suspense fallback={<Loading />}>{children}</Suspense>
+            {children}
           </div>
         </main>
         <Footer />
