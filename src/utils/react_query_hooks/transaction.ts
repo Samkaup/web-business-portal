c419ecd1581ebc47380825b69c096d429f6e0091
Mimmi <bjarmiae@gmail.com>
2023-08-22 10:59:14 +0000

    feat: add filtered, paginated and sorted query

--- src/utils/react_query_hooks/transaction.ts
+++ src/utils/react_query_hooks/transaction.ts
@@ -1,10 +1,66 @@
 import { TableRow } from '@/types';
 import { useQuery } from '@tanstack/react-query';
 import supabaseClient from '@/utils/supabase-browser';
-import { getTransactions } from '../supabase_queries/transaction';
+import {
+  getTransactions,
+  getTransactionsTable,
+} from '../supabase_queries/transaction';
+import { QueryDataAndCount } from '../utilTypes';
+import { PaginationState, SortingState } from '@tanstack/react-table';
 
 export const useTransactions = () => {
-  return useQuery<TableRow<'transaction'>[]>(['transaction'], async () => {
+  return useQuery<TableRow<'transaction'>[]>(['transactions'], async () => {
     return getTransactions(supabaseClient);
   });
+};
+
+type Payload = {
+  pagination?: PaginationState;
+  sorting?: SortingState;
+  searchValue?: string | string[];
+  dateRange?: Date[];
+  filters?: string[];
+};
+
+export const useTransactionsTable = ({
+  pagination,
+  sorting,
+  searchValue,
+  dateRange,
+  filters,
+}: Payload) => {
+  return useQuery<QueryDataAndCount<TableRow<'transaction'>>>(
+    [
+      'transactionsTable',
+      {
+        pagination,
+        sorting,
+        searchValue,
+        dateRange,
+        filters,
+      },
+    ],
+    async () => {
+      const rangeFrom = pagination.pageIndex * pagination.pageSize;
+      const rangeTo = (pagination.pageIndex + 1) * pagination.pageSize - 1;
+
+      return getTransactionsTable({
+        supabaseClient,
+        range: {
+          from: rangeFrom,
+          to: rangeTo,
+        },
+        sorting: {
+          column: sorting[0].id,
+          options: {
+            ascending: !sorting[0].desc,
+          },
+        },
+        searchValue,
+        dateRange,
+        filters,
+      });
+    },
+    { keepPreviousData: true }
+  );
 };
--- src/utils/supabase_queries/transaction.ts
+++ src/utils/supabase_queries/transaction.ts
@@ -1,4 +1,6 @@
-import { AppSupabaseClient, TableRow } from '@/types';
+import { AppSupabaseClient, Row, TableRow } from '@/types';
+import { RangeProps, SortingProps } from '@/utils/utilTypes';
+import { formatDate } from '../dateUtils';
 
 export const getTransactions = async (
   supabase: AppSupabaseClient
@@ -27,4 +29,57 @@ export const getTransactionCount = async (
     throw error;
   }
   return count;
+};
+
+export type Payload = {
+  supabaseClient: AppSupabaseClient;
+  range?: RangeProps;
+  sorting?: SortingProps;
+  searchValue?: string | string[];
+  dateRange?: Date[];
+  filters?: string[];
+};
+
+export const getTransactionsTable = async ({
+  supabaseClient,
+  range,
+  sorting,
+  searchValue,
+  dateRange,
+  filters,
+}: Payload) => {
+  let query = supabaseClient
+    .from('transaction')
+    .select('*', { count: 'exact' });
+
+  if (range) query = query.range(range.from, range.to);
+
+  if (sorting)
+    query = query.order(sorting.column as string & keyof Row<'transaction'>, {
+      ascending: sorting.options.ascending,
+    });
+
+  if (searchValue || filters) {
+    let filterString: string = filters ? filters.join() : '';
+
+    if (searchValue)
+      filterString = `${filterString}, contact_name.ilike.*${searchValue}*, department_name.ilike.*${searchValue}*`;
+
+    if (filterString.length > 0) query = query.or(filterString);
+  }
+
+  // Set Date Range
+  const start: Date | null = dateRange.at(0);
+  const end: Date | null = dateRange.at(1);
+  query = start ? query.filter('date', 'gte', formatDate(start)) : query;
+  query = end ? query.filter('date', 'lte', formatDate(end)) : query;
+
+  const { data, count, error } = await query;
+
+  if (error) {
+    console.error(error.message);
+    throw error;
+  }
+
+  return { data, rowCount: count };
 };
