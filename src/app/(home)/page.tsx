06b89730eb4d2d9ff11609782fb5a4cc41ef8103
Mimmi <bjarmiae@gmail.com>
2023-12-08 12:49:06 +0000

    feat: change header to except title prop

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,16 @@
+'use client';
+
 import Dashboard from '@/components/Dashboard/Dashboard';
 import Header from '@/components/Header/Header';
+import { useContext } from 'react';
+import { Context } from '@/utils/context-store';
 
 export default function HomePage() {
+  const { company } = useContext(Context);
+
   return (
     <>
-      <Header />
+      <Header title={company.name} />
       <div className="pt-10">
         <Dashboard />
       </div>
