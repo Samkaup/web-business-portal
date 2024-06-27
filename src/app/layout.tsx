34bc82403e743007a7cca1aa5c6f4e9d7d84a4c7
Pétur Karl  Ingólfsson <petur.karl.ingolfsson@gmail.com>
2024-06-27 17:01:00 +0000

    feat: added Analytics to Mínar Síður

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { ClientLayout } from '@/components/ClientLayout';
 import './globals.css';
 export const revalidate = 0;
@@ -12,6 +13,7 @@ export default async function RootLayout({
       <head />
       <body className="h-full">
         <ClientLayout>{children}</ClientLayout>
+        <Analytics />
       </body>
     </html>
   );
